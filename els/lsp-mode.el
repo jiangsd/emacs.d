9b6dedd705195d473184581372416465c399ae7a
jiangsdex <jiangsd@yahoo.com>
2020-05-02 00:05:39 +0800

    lsp changes  On branch wolf  Your branch is up to date with 'origin/wolf'.
     Changes to be committed:
    	new file:   .dap-breakpoints
    	new file:   .lsp-session-v1
    	modified:   els/lsp-mode.el

--- els/lsp-mode.el
+++ els/lsp-mode.el
@@ -5,7 +5,7 @@
 
 (use-package yasnippet :ensure t)
 (use-package lsp-mode :ensure t)
-(use-package company-lsp :ensure t)
+;; (use-package company-lsp :ensure t)
 (use-package lsp-ui :ensure t)
 (use-package lsp-java :ensure t :after lsp
   :config (add-hook 'java-mode-hook 'lsp))
